2987a788abb40f620bea1262c8ba190bc71a602f
Kirill Salnikov <salnikov.k54@gmail.com>
2025-05-26 14:26:53 +0700

    feat: hypr lock screen

--- laptop/home/.config/fish/functions/gitchanges.fish
+++ laptop/home/.config/fish/functions/gitchanges.fish
@@ -1,4 +1,3 @@
 function gitchanges --wraps='git log --pretty=format: --name-only | sort | uniq -c | sort -rg' --description 'alias gitchanges=git log --pretty=format: --name-only | sort | uniq -c | sort -rg'
   git log --pretty=format: --name-only | sort | uniq -c | sort -rg $argv
-        
 end
