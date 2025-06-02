728e2605e25e35bc2ea3d55439afb3890fd262ba
Kirill Salnikov <salnikov.k54@gmail.com>
2025-06-03 02:21:17 +0300

    feat(nvim): add copilot chat plugin

--- laptop/home/.config/nvim/lua/keiqu/plugins/copilot.lua
+++ laptop/home/.config/nvim/lua/keiqu/plugins/copilot.lua
@@ -32,4 +32,15 @@ return {
     },
     init = function() vim.g.copilot_proxy = os.getenv("COPILOT_PROXY_URL") end,
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+  },
 }
