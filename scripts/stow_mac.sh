fb96f730c00ce1f5712863381ad558e94719de5b
Kirill Salnikov <salnikov.k54@gmail.com>
2025-10-31 14:40:55 +0300

    feat: small tweaks for mac

--- scripts/stow_mac.sh
+++ scripts/stow_mac.sh
@@ -3,5 +3,8 @@
 set SCRIPT_DIR (dirname (realpath (status -f)))
 set STOW_DIR (dirname $SCRIPT_DIR)
 
+echo "Stowing common configs"
 stow --adopt -d $STOW_DIR/common -t $HOME -v home
+
+echo "Stowing mac configs"
 stow --adopt -d $STOW_DIR/macbook -t $HOME -v home
