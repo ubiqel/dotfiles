ce0b426d2d7427110048f5798fb8c854409e851c
Kirill Salnikov <salnikov.k54@gmail.com>
2026-01-05 01:05:58 +0300

    feat(fish): remove unneeded script

--- common/home/.config/fish/functions/is_playing.fish
+++
@@ -1,3 +0,0 @@
-# TODO: get list of player's statuses via playerctl status -a each line will
-# contain status "Playing" or "Paused". If any of the lines contains "Playing"
-# (one of the players is playing), then return "true". Else return "false"
