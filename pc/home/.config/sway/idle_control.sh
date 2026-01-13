3145d6a7376387b53471ad96a409e2b91b984320
Kirill Salnikov <salnikov.k54@gmail.com>
2026-01-13 10:12:40 +0300

    feat(sway): fix swayidle

---
+++ pc/home/.config/sway/idle_control.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+is_playing() {
+    playerctl status -a 2>/dev/null | grep -q Playing
+}
+
+case "$1" in
+    lock)
+        exec swaylock -f -k --color 000000 --indicator-idle-visible
+        ;;
+
+    lock-if-idle)
+        if ! is_playing; then
+            exec swaylock -f -k --color 000000 --indicator-idle-visible
+        fi
+        ;;
+
+    dpms-off-if-idle)
+        if ! is_playing; then
+            exec swaymsg "output * dpms off"
+        fi
+        ;;
+
+    dpms-on)
+        exec swaymsg "output * dpms on"
+        ;;
+
+    *)
+        exit 1
+        ;;
+esac
